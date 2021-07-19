7610f69328a7a1aac9f68377ab308fad6acda5bb
angelawadden <87436934+angelawadden@users.noreply.github.com>
2021-07-19 11:28:23 -0400

    Completed Iteration 0

---
+++ ToDoList/ToDoList/ToDo.swift
@@ -0,0 +1,14 @@
+//
+//  ToDo.swift
+//  ToDoList
+//
+//  Created by Angela Wadden on 7/19/21.
+//
+
+import UIKit
+
+class ToDoClass {
+    
+    var description = ""
+    var important = false 
+}
